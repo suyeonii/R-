ec2dc037a92a92489b61a837ebee7369671910b8
suyeonii <tallvoy@naver.com>
2019-05-29 15:11:56 +0900

    Part3, Section1-3

---
+++ Part3/sec1-3.R
@@ -0,0 +1,19 @@
+setwd("d:/workspace/TIL/R_Data_Analysis/Part3/data")
+getwd()
+
+print(1+2)
+1+2
+print('a')
+"a"
+'string'
+print(pi)
+print(pi, digits=3)
+print(pi, digits=4)
+print(3,4) # 하나만 찍힌다
+print(1,2)
+print('a','b') # 에러가 발생한다
+cat(1,';','a','/n',1,':','b')
+cat(1,';','a','\n',2,':','b')
+3,4
+3; 4; 5;
+1+2; 3*4; pi*5*5; 4-5;
